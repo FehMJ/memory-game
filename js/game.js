e93b89a95ac034045517606f8630ed131af5074a
Felipe <127160057+FehMJ@users.noreply.github.com>
2023-04-16 18:34:39 -0300

    memory-game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,48 @@
-const createCard = () => {
-    const card = document.createElement('div');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+const grid = document.querySelector('.grid');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
 }
+
+const createCard = (character) => {
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../imagens/${character}.png')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+}
+
+const loadGame = () => {
+
+    const duplicateCharacters = [ ...characters, ...characters ];
+    
+    duplicateCharacters.forEach((character) => {
+
+        const card = createCard(character);
+        grid.appendChild(card);
+
+    });
+}
+
+loadGame();
