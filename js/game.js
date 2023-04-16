b6d7e620f56d88384bc6ac8edec4620c9ff11475
Felipe <127160057+FehMJ@users.noreply.github.com>
2023-04-16 20:47:40 -0300

    memory-game

--- js/game.js
+++ js/game.js
@@ -19,6 +19,64 @@ const createElement = (tag, className) => {
     return element;
 }
 
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length === 20) {
+        alert('Parabéns, você conseguiu!');
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+  
+    if (firstCharacter === secondCharacter) {
+  
+      firstCard.firstChild.classList.add('disabled-card');
+      secondCard.firstChild.classList.add('disabled-card');
+  
+      firstCard = '';
+      secondCard = '';
+  
+      checkEndGame();
+  
+    } else {
+      setTimeout(() => {
+  
+        firstCard.classList.remove('reveal-card');
+        secondCard.classList.remove('reveal-card');
+  
+        firstCard = '';
+        secondCard = '';
+  
+      }, 500);
+    }
+  
+  }
+
+const revealCard = ( {target} ) => {
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+    if (firstCard == '') {
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+
+    } else if (secondCard == '') {
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+
+}
+
 const createCard = (character) => {
 
     const card = createElement('div', 'card');
@@ -30,12 +88,23 @@ const createCard = (character) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-character', character)
+
     return card;
 }
 
 const loadGame = () => {
 
     const duplicateCharacters = [ ...characters, ...characters ];
+
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+    shuffledArray.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+    });
+    
     
     duplicateCharacters.forEach((character) => {
 
